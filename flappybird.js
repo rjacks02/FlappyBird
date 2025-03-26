f6c67cc73767971d983a065b51c2f2c704204d81
Rebecca Jackson <rjacks02@villanova.edu>
2025-03-26 14:08:26 -0400

    Switch to touchend

--- flappybird.js
+++ flappybird.js
@@ -65,7 +65,7 @@ window.onload = function() {
     setInterval(updateBirdFrameID, 500);
     document.addEventListener("keydown", moveBird);
     document.addEventListener("mousedown", moveBird);
-    document.addEventListener("touchstart", moveBird, { passive: false });
+    document.addEventListener("touchend", moveBird);
 }
 
 function update() {
@@ -159,7 +159,7 @@ function updateBirdFrameID(){
 }
 
 function moveBird (e) {
-    if (e.type == "mousedown" || e.type == "touchstart" || e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX"){
+    if (e.type == "mousedown" || e.type == "touchend" || e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX"){
         //jump
         velocityY = -6;
 
