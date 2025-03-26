bed3b010599f2ff7cda0d12f81cab6c6c1baf5ea
Rebecca Jackson <rjacks02@villanova.edu>
2025-03-25 21:34:51 -0400

    Add bird to canvas

--- flappybird.js
+++ flappybird.js
@@ -4,10 +4,32 @@ let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
+//bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth/8;
+let birdY = boardHeight/2;
+let birdImg;
+
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight
+}
+
 window.onload = function() {
+    //context
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
-    context = board.getContext("2d"); 
-
+    context = board.getContext("2d");
+    
+    //bird
+    birdImg = new Image();
+    birdImg.src = "./flappybird.png";
+    birdImg.onload = function() {
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    }
+    
 }
