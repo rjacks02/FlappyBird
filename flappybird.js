18d3444d4e5f68a9abe10c145197ea2c3f07474c
Rebecca Jackson <rjacks02@villanova.edu>
2025-03-25 21:55:41 -0400

    Add moving bottom pipe

--- flappybird.js
+++ flappybird.js
@@ -71,15 +71,29 @@ function update() {
 }
 
 function placePipes(){
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2);
+    let openingSpace = board.height/4;
+
     let topPipe = {
         img : topPipeImg,
         x : pipeX,
-        y : pipeY,
+        y : randomPipeY,
         width : pipeWidth,
         height : pipeHeight,
         passed : false
     }
 
     pipeArray.push(topPipe);
+
+    let bottomPipe = {
+        img : bottomPipeImg,
+        x : pipeX,
+        y : randomPipeY + pipeHeight + openingSpace,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+
+    pipeArray.push(bottomPipe);
 
 }
